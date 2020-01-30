36c62d495cfc074d4ecb268ae2bd6b5fcf652e45
Bryn-Weed <brynweed@gmail.com>
2020-01-30 13:33:39 -0500

    s

--- src/Header.js
+++ src/Header.js
@@ -7,16 +7,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 class Header extends Component {
   render() {
     return (
-      <section>
         <div className="navbar">
-          <a href="#top">Home</a>
-          <a href="#about">About</a>
-          <a href="#contact">Contact</a>
+          <div className="page-links">
+            <a href="#top">Home</a>
+            <a href="#about">About</a>
+            <a href="#contact">Contact</a>
+          </div>
           <div className="login">
             <button type="button" className="btn btn-info btn-sm" data-toggle="modal" data-target="#myModal">Login</button>
           </div>
-        </div>
-      </section>
+        </div>      
     )
   }
 }
