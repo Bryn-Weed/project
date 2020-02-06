4e6ad8e4a381321def617f05660b4b10e33a5f13
Bryn-Weed <brynweed@gmail.com>
2020-02-06 08:35:23 -0500

    Login UI improvements
    Added forgot password button,
    changed layout for usability and dynamic resolutions

--- src/Login.js
+++ src/Login.js
@@ -38,26 +38,29 @@ class Login extends React.Component{
     return(
     <div className="background">
       <div style={{ textAlign : 'center' }}>
-        <div className="col-lg-2 col-centered">
+        <div className="col-lg-2.5 col-centered login-card">
           <form>
-            <h1 style={{float: 'left', marginBottom: '25px'}}>Welcome!</h1>
+            <h1 style={{float: 'left', marginBottom: '25px', marginLeft: '27px'}}>Welcome!</h1>
 
-            <div className="input-group mb-3" >
+            <div className="input-group mb-4 col-xs-5 w-75 col-centered">
               <div className="input-group-prepend">
                 <span className="glyphicon glyphicon-user input-group-text" aria-hidden="true" id="icon" style={{fontSize: '15px', top: '0px'}}></span>
               </div>
               <input id="email" placeholder="email" type="text" style={{fontSize: '15px'}} className="form-control"/>
             </div>
 
-            <div className="input-group mb-3">
+            <div className="input-group mb-4 col-xs-5 w-75 col-centered">
               <div className="input-group-prepend">
                 <span className="glyphicon glyphicon-lock input-group-text" aria-hidden="true" id="icon" style={{fontSize: '15px', top: '0px'}}></span>
               </div>
               <input id="password" placeholder="password" type="password" style={{fontSize: '15px'}} className="form-control"/>
             </div>
 
-            <button style = {{margin : '10px', fontSize: '15px'}} onClick = {this.register} className="btn btn-dark btn float-right">Sign Up</button>
+
+            <button style = {{margin : '10px', fontSize: '15px', marginRight: '40px'}} onClick = {this.register} className="btn btn-dark btn float-right">Sign Up</button>
             <button style = {{margin : '10px', fontSize: '15px'}} onClick = {this.login} className="btn btn-primary btn float-right">Log In</button>
+
+            <a style={{fontSize: '12px', display: 'block', float: 'right', marginTop: '65px'}} href="#">Forgot your password?</a>
 
           </form>
         </div>
